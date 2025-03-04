6dde58950252edeb0ec8b35c42d8a022c1d99a2a
Amal Pradeep <amalpradeep271@gmail.com>
2025-03-04 21:54:29 +0530

    added favourire icon button

---
+++ lib/common/widgets/app_button/favourite_button.dart
@@ -0,0 +1,34 @@
+import 'package:e_commerce_application/common/bloc/button/favourite_icon_cubit.dart';
+import 'package:e_commerce_application/core/configs/theme/app_colors.dart';
+import 'package:e_commerce_application/domain/product/entity/product_entity.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class FavoriteButton extends StatelessWidget {
+  final ProductEntity productEntity;
+  const FavoriteButton({required this.productEntity, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        context.read<FavoriteIconCubit>().onTap(productEntity);
+      },
+      icon: Container(
+        height: 40.h,
+        width: 40.w,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+        ),
+        child: BlocBuilder<FavoriteIconCubit, bool>(
+          builder: (context, state) => Icon(
+            state ? Icons.favorite : Icons.favorite_outline,
+            size: 18,
+            color: AppColors.red,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/common/widgets/product/product_card.dart
+++ lib/common/widgets/product/product_card.dart
@@ -1,8 +1,11 @@
 import 'package:e_commerce_application/common/helper/images/images_display.dart';
+import 'package:e_commerce_application/common/widgets/app_button/favourite_button.dart';
 import 'package:e_commerce_application/core/configs/theme/app_colors.dart';
 import 'package:e_commerce_application/core/configs/theme/app_text_theme.dart';
 import 'package:e_commerce_application/domain/product/entity/product_entity.dart';
+import 'package:e_commerce_application/common/bloc/button/favourite_icon_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 // class ProductCard extends StatelessWidget {
@@ -112,66 +115,65 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        GestureDetector(
-            onTap: () {},
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 190.h,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5.r),
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        ImageDisplayHelper.generateProductImageURL(
-                          productEntity.images[0],
+    return BlocProvider(
+      create: (context) =>
+          FavoriteIconCubit()..isFavorite(productEntity.productId),
+      child: Stack(
+        children: [
+          GestureDetector(
+              onTap: () {},
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 190.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.r),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          ImageDisplayHelper.generateProductImageURL(
+                            productEntity.images[0],
+                          ),
                         ),
+                        fit: BoxFit.fill,
                       ),
-                      fit: BoxFit.fill,
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  productEntity.title,
-                  overflow: TextOverflow.ellipsis,
-                  style: AppTextStyles.base.w500.s15,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      productEntity.discountPrice == 0
-                          ? "₹ ${productEntity.price}"
-                          : "₹ ${productEntity.discountPrice}",
-                      style: AppTextStyles.base.w700.s15,
-                    ),
-                    SizedBox(
-                      width: 10.w,
-                    ),
-                    Text(
-                      productEntity.discountPrice == 0
-                          ? ''
-                          : "₹ ${productEntity.price}",
-                      style: AppTextStyles.base.s15.greyColor.w300.lineThrough,
-                    ),
-                  ],
-                )
-              ],
-            )),
-        Align(
-          alignment: Alignment.topRight,
-          child: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.favorite,
-                color: AppColors.grey,
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    productEntity.title,
+                    overflow: TextOverflow.ellipsis,
+                    style: AppTextStyles.base.w500.s15,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        productEntity.discountPrice == 0
+                            ? "₹ ${productEntity.price}"
+                            : "₹ ${productEntity.discountPrice}",
+                        style: AppTextStyles.base.w700.s15,
+                      ),
+                      SizedBox(
+                        width: 10.w,
+                      ),
+                      Text(
+                        productEntity.discountPrice == 0
+                            ? ''
+                            : "₹ ${productEntity.price}",
+                        style:
+                            AppTextStyles.base.s15.greyColor.w300.lineThrough,
+                      ),
+                    ],
+                  )
+                ],
               )),
-        )
-      ],
+          Align(
+              alignment: Alignment.topRight,
+              child: FavoriteButton(productEntity: productEntity))
+        ],
+      ),
     );
   }
 }
